96670bc210c2a1dd8403265a270a4d9928284d95
Jessica Amy <jessicaannetteamy@gmail.com>
2025-05-17 16:21:02 +0930

    Added checkUserType which stores a value of user or admin as a string and created auth_routes

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,36 @@
-test
+// this is the auth middle were 
+// will check if a user is of type admin or user
+
+import { expressjwt } from "express-jwt"
+import User from './models/user.js'
+
+// auth middleware function auth takes in params of req, rest, next
+
+export function auth(req, res, next) {
+    return expressjwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] })(req, res, next)
+}
+// checkUserType function checks if the user is an admin or a regular user
+
+
+export function checkUserType(req, res, next) {
+    if (req.auth) {
+
+        // finds by the email then returns if its a user or admin
+        // we use the findOne method to find the user by email
+
+        User.findOne({ email: req.auth.email }).then(user => {
+            if (user && user.accountType === 'admin') {
+                req.userType = 'admin'
+                res.status(200).send({ message: 'Authenticated Admin' })
+            } else {
+                res.status(200).send({ message: 'Authenticated User' })
+                req.userType = 'user'
+            }
+            next()
+        })
+    } else {
+        res.status(403).send({ error: 'Unauthorized' })
+    }
+}
+
+export default { auth, checkUserType }
