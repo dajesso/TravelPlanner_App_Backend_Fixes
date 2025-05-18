ab0c08f54bb4dcafe473d5dcb40e9eee0c684ebd
Angie <angel890817@hotmail.com>
2025-05-18 12:40:11 +1000

    feat made category name unique and Post/ Get one category works

--- src/models/category.js
+++ src/models/category.js
@@ -2,7 +2,7 @@ import { model } from 'mongoose'
 
 
 const Category = model('Category', {
-    name: { type: String, required: true }
+    name: { type: String, required: true, unique: true }
 })
 
 export default Category
--- src/routes/category_routes.js
+++ src/routes/category_routes.js
@@ -7,6 +7,45 @@ const router = Router()
 //get all category
 router.get('/categories', async(req, res)=> {
     res.send(await Category.find());
+});
+
+//Get one category
+router.get('/categories/:id', async(req, res) => {
+    // Get the id
+    const category_id = req.params.id // string
+
+    const category = await Category.findById(category_id);
+
+    //send the post back to the client
+    if (category) {
+        res.send(category)
+
+    } else{
+        res.status(404).send ({error:`Category with id ${category_id} not found`})
+    }
+
+});
+
+// Create category
+router.post('/categories', async(req,res) => {
+    try {
+        // Get data from the request body abd check if the name is unique
+        const exists = await Category.findOne({ name: req.body.name });
+        // if the category name already exists
+        if (exists) {
+        return res.status(400).send({ error: 'Category name already exists' });
+        }
+        // Create and save new category
+        const category = await Category.create(bodyData)
+        // Send post to the client with 201 status
+        res.status(201).send(category)
+    }
+    catch (err) {
+        // TODO: Log to error file
+        res.status(400).send({ error: err.message })
+    }
 })
+
+
 
 export default router
