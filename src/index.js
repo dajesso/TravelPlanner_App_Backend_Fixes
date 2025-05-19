8da194bac4abb39792864f45a15ff872be9650fd
hernan-vela <168710538+hernan-vela@users.noreply.github.com>
2025-05-19 21:14:07 +1000

    feat: creation of trip model and trip_routes.js
    Done:
    - Creation of trip model (schema) and trip_routes.js with full CRUD operation
    - Import of Trip model and route into index.js
    - Addition of db.js. This file might be needed to connect/disconnect automatically from the database
    
    Pending: back populates trip_routes from expense and category models, and complete all tests for the routes.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import trip_routes from './routes/trip_routes.js'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// middleware to parse JSON body from the client
+app.use(express.json())
+
+
+// insertion of middleware through respective routes
+app.use(trip_routes)
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`) 
+  // Connect to MongoDB
+  mongoose.connect('mongodb://127.0.0.1:27017/latte_dB')
+  console.log(mongoose.connection)
 })
